1d42756fb8ade81f959654429bfe3ceb60e5f6c4
kedpak <kedpak@yahoo.com>
2017-06-09 04:24:19 +0000

    fixed base models file

---
+++ models/engine/file_storage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    
+    def __init__(self):
+        self.__file_path = file
+        self.__objects = {}
+        BaseModel.id = 12121212
+
+print(BaseModel.id)

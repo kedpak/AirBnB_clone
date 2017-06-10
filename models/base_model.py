efe721141d8080e571ea02b593c12b3af906f143
kedpak <kedpak@yahoo.com>
2017-06-10 07:08:21 +0000

    added storage file

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ creates class BaseModel which defines all common attributes/methods
 for other classes
 '''
 from datetime import datetime
+from models import storage
 import uuid
 
 
@@ -24,22 +25,26 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
+            storage.new(self)
 
     def save(self):
         ''' public method:
         updates current datetime
         '''
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_json(self):
         ''' public method:
         returns dictionary containing all key/values
         '''
-        self.created_at = str(self.created_at.isoformat())
+        copy_dict = self.__dict__.copy()
+        copy_dict["__class__"] = self.__class__.__name__
+        copy_dict["created_at"] = str(self.created_at.isoformat())
         if hasattr(self, "updated_at") == True:
-            self.updated_at = str(self.updated_at.isoformat())
-        self.__dict__["__class__"] = self.__class__.__name__
-        return (self.__dict__)
+            copy_dict["updated_at"] = str(self.updated_at.isoformat())
+
+        return (copy_dict)
 
     def __str__(self):
         '''print result
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,19 +1,44 @@
 #!/usr/bin/python3
 
-from models.base_model import BaseModel
-
+#from models.base_model import BaseModel
+import json
+import os
 
 class FileStorage:
     
     def __init__(self):
-        self.__file_path = "file.json"
+        '''constructor method
+        '''
+        self.__file_path = 'file.json'
         self.__objects = {}
-        BaseModel.id = 12121212
 
     def all(self):
-        return (__objects)
+        '''returns dictionary
+        '''
+        return (self.__objects)
 
     def new(self, obj):
-        
+        '''assigns the object id to  obj'''
+        self.__objects[obj.id] = obj
 
-print(BaseModel.id)
+    def save(self):
+        '''serialize __object to JSON file
+        '''
+        j_dict = {}
+        print(self.__objects)
+        for key in self.__objects.keys():
+            j_dict[key] = self.__objects[key].to_json()
+        with open(self.__file_path, mode='w') as f:
+            f.write(json.dumps(j_dict, sort_keys = True, indent = 4))
+
+
+    def reload(self):
+        '''deserialze the JSON file to __objects
+        '''
+        from models.base_model import BaseModel
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, mode = 'r') as f:
+                j_load = json.load(f)
+                print("after load")
+                for key in j_load.keys():
+                    self.__objects[key] = BaseModel(**j_load[key])

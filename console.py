b810bf59286344edd06ea54c7cf0423177427ff2
kedpak <kedpak@yahoo.com>
2017-06-11 23:29:29 +0000

    added all method to console

--- console.py
+++ console.py
@@ -42,33 +42,51 @@ class HBNBCommand(cmd.Cmd):
         all_obj = storage.all()
         arg_list = list(args.split())
 
-        if all_obj["BaseModel." + self.model_id] is None:
-            print("** no instance found **")
-        if arg_list[0] == "BaseModel" and arg_list[1] == self.model_id:
-            print(all_obj["BaseModel." + self.model_id])
+        if arg_list[0] == "BaseModel" and arg_list[1]:
+            if ("BaseModel." + self.model_id) not in all_obj:
+                print ("** no instance found **")
+                return
+            if arg_list[1] == self.model_id:
+                print(all_obj["BaseModel." + self.model_id])
+            else:
+                print("** instance id missing **")
         elif arg_list[0] != "BaseModel":
             print("** class doesn't exist **")
-        elif arg_list[1] != self.model_id:
-            print("** instance id missing **")
-
     
     def do_destroy(self, args):
         """ deletes instance based on class name and id
         """
-        if len(args) ==0:
+        if len(args) == 0:
             print("** class name missing **")
             return
         all_obj = storage.all()
         arg_list = list(args.split())
 
         if arg_list[0] == "BaseModel" and arg_list[1] == self.model_id:
-            del(self.b_model)
+            del(all_obj["BaseModel." + self.model_id])
         elif arg_list[0] != "BaseModel":
             print("** class doesn't exist **")
         elif arg_list[1] != self.model_id:
             print("** instance id missing **")
 
-    
+
+    def do_all(self, args):
+        """ prints all string representation of instances created
+        """
+        all_obj = storage.all()
+        arg_list = list(args.split())
+
+        if len(arg_list) == 0:
+            for obj in all_obj:
+                print(all_obj[obj])
+                return
+        if arg_list[0] == "BaseModel":
+            for obj in all_obj:
+                print(all_obj[obj])
+
+        
+
+
     def emptyline(self):
         '''if empty line entered pass
         '''

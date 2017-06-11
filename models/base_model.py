5f5305aa4db6e68e4041905ff37ea89764accca2
clturner <clturner23@gmail.com>
2017-06-11 06:15:56 +0000

    base model class

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,38 @@
 #!/usr/bin/python3
 import uuid
-from datetime import datetime
+import datetime
 import json
+from models import storage
 
+format = "%Y-%m-%dT%H:%M:%S.%f"
 class BaseModel:
+    format = "%Y-%m-%dT%H:%M:%S.%f"
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = datetime.now().isoformat()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            if kwargs.get('id') is not None:
+                self.__dict__ = kwargs
+            self.created_at = datetime.datetime.strptime(kwargs.get('created_at'), format)
+            self.__dict__ == kwargs
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            storage.new(self)
 
     def save(self):
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_json(self):
-        self.created_at = str(datetime.now().isoformat(sep="T"))
-        self.updated_at = str(datetime.now().isoformat(sep="T"))
-        self.__dict__["__class__"] = self.__class__.__name__
-        return(self.__dict__)
+        new_dict = self.__dict__.copy()
+        new_dict.update({'created_at': self.created_at.strftime(self.format)})
+        new_dict.update({"__class__":str(self.__class__.__name__)})
+        if hasattr(self, 'updated_at'):
+            new_dict['updated_at'] = str(self.updated_at)
+        else:
+            pass
+        return new_dict
 
     def __str__(self):
         "returns [<class name>] (<self.id>) <self.__dict__>"
-        return('[{}] ({}) {}'.format(self.__class__.__name__,self.id, self.__dict__))
+        return('[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__))

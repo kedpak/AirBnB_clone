71f93a2d1490a22be641cee79e8c8b08e4dff289
kedpak <kedpak@yahoo.com>
2017-06-11 11:03:56 +0000

    added create, show, and commands to console

--- console.py
+++ console.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import cmd
+from models.base_model import BaseModel
+from models import storage
 '''module: console
 This module contains the entry point of command line iterpreter
 Includes: help(builtin), EOF, quit, and custom prompt('hbnb')
@@ -14,11 +16,65 @@ class HBNBCommand(cmd.Cmd):
         cmd.Cmd.__init__(self)
         self.prompt = "(hbnb) "
 
+    def do_create(self, args):
+        """ creates a new instance of class BaseModel
+        """
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+
+        arg_list = list(args.split())
+        if arg_list[0] == "BaseModel":
+            b_model = BaseModel()
+            b_model.save()
+            self.model_id = b_model.id
+            print('{0}'.format(b_model.id))
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, args):
+        """ print string representation of instance based on
+        class name and ID """
+
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        all_obj = storage.all()
+        arg_list = list(args.split())
+
+        if all_obj["BaseModel." + self.model_id] is None:
+            print("** no instance found **")
+        if arg_list[0] == "BaseModel" and arg_list[1] == self.model_id:
+            print(all_obj["BaseModel." + self.model_id])
+        elif arg_list[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif arg_list[1] != self.model_id:
+            print("** instance id missing **")
+
+    
+    def do_destroy(self, args):
+        """ deletes instance based on class name and id
+        """
+        if len(args) ==0:
+            print("** class name missing **")
+            return
+        all_obj = storage.all()
+        arg_list = list(args.split())
+
+        if arg_list[0] == "BaseModel" and arg_list[1] == self.model_id:
+            del(self.b_model)
+        elif arg_list[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif arg_list[1] != self.model_id:
+            print("** instance id missing **")
+
+    
     def emptyline(self):
         '''if empty line entered pass
         '''
         pass
 
+    
     def do_EOF(self, line):
         '''EOF command to exit the program
         '''

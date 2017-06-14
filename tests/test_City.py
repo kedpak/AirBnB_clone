e48eba419dfcc067f8ae4a46bd36782d33119ce3
clturner <clturner23@gmail.com>
2017-06-14 05:18:12 +0000

    change names to city

--- tests/test_City.py
+++ tests/test_City.py
@@ -7,25 +7,25 @@ from models.test_City import City
 Class test_City(unittest.TestCase):
 
     def setUp(self):
-        self.amenity = Amenity()
+        self.city = City()
 
     def test_instance(self):
-        self.assertIsInstance(self.state, City)
+        self.assertIsInstance(self.city, City)
 
     def test_attributes(self):
-        self.assertTrue(hasattr(self.City, "created_at"))
-        self.assertTrue(hasattr(self.City, "updated_in"))
-        self.assertTrue(hasattr(self.City, "id"))
-        self.assertTrue(hasattr(self.City, "name"))
+        self.assertTrue(hasattr(self.city, "created_at"))
+        self.assertTrue(hasattr(self.city, "updated_in"))
+        self.assertTrue(hasattr(self.city, "id"))
+        self.assertTrue(hasattr(self.city, "name"))
         self.assertEqual(hasattr(self.__class__.__name__, "City"))
 
     def test_update(self):
-        self.state.save()
-        self.assertTrue(hasattr(self.amenity, "updated_at"))
+        self.city.save()
+        self.assertTrue(hasattr(self.city, "updated_at"))
 
     def test_Save(self):
         self.city.save()
-        self.assertTrue(hasattr(self.amenity, "updated_at"))
+        self.assertTrue(hasattr(self.city, "updated_at"))
 
 if __name__ == "__main__":
     unittest.main
